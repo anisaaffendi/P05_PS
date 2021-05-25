b130273cdab6df194f8a03f3b2d3edaed662809a
Stun4Life <weeweiren@gmail.com>
2021-05-25 14:41:18 +0800

    Anisa is our leader

--- app/src/main/java/sg/edu/rp/c347/id19030019/smsretriever/FragmentSecond.java
+++ app/src/main/java/sg/edu/rp/c347/id19030019/smsretriever/FragmentSecond.java
@@ -2,6 +2,7 @@ package sg.edu.rp.c347.id19030019.smsretriever;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,7 +25,7 @@ public class FragmentSecond extends Fragment {
     private static final String TAG = FragmentSecond.class.getSimpleName();
 
     // Views
-    private Button btnAddtext2;
+    private Button btnAddtext2, btnSendEmail;
     private TextView tvFrag2;
     private EditText etFrag2;
 
@@ -40,6 +41,7 @@ public class FragmentSecond extends Fragment {
         etFrag2 = view.findViewById(R.id.etFrag2);
         btnAddtext2 = view.findViewById(R.id.btnFrag2);
         tvFrag2 = view.findViewById(R.id.tvFrag2);
+        btnSendEmail = view.findViewById(R.id.btnSendEmail);
 
         btnAddtext2.setOnClickListener(v-> {
             Uri uri = Uri.parse("content://sms");
@@ -97,6 +99,46 @@ public class FragmentSecond extends Fragment {
                 return;
         });
 
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri uri = Uri.parse("content://sms");
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+                ContentResolver cr = getContext().getContentResolver();
+
+                if (!etFrag2.getText().toString().trim().isEmpty()) {
+                    String filter = "body LIKE ?";
+                    String[] filterArgs = {"%"+etFrag2.getText().toString().trim()+"%"};
+
+                    Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                    String smsBody = "";
+                    if (cursor.moveToFirst()) {
+                        do {
+                            long dateInMillis = cursor.getLong(0);
+                            String date = (String) DateFormat.format("dd MM yyyy h:mm:ss aa", dateInMillis);
+                            String address = cursor.getString(1);
+                            String body = cursor.getString(2);
+                            String type = cursor.getString(3);
+                            if (type.equalsIgnoreCase("1"))
+                                type = "Inbox:";
+                            else
+                                type = "Sent:";
+                            smsBody += type + " " + address + "\n at" + date + "\n\"" + body + "\"\n\n";
+                        } while (cursor.moveToNext());
+                    }
+                    Intent email = new Intent(Intent.ACTION_SEND);
+                    email.putExtra(Intent.EXTRA_EMAIL, new String[]{"weeweiren@gmail.com"});
+                    email.putExtra(Intent.EXTRA_SUBJECT, "Your past SMS");
+                    email.putExtra(Intent.EXTRA_TEXT, smsBody);
+                    email.setType("message/rfc822");
+                    startActivity(Intent.createChooser(email, "Choose an Email client :"));
+                } else
+                    return;
+            }
+        });
+
         return view;
+
     }
 }
